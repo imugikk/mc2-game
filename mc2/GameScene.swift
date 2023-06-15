bf4e5e2c55a1c7aa6607052d80ccf4c84c38ad00
Michelle Annice <michelle.annice@ui.ac.id>
2023-06-15 10:55:23 +0700

    change score and high score from GameScene.sks

--- mc2/GameScene.swift
+++ mc2/GameScene.swift
@@ -29,100 +29,17 @@ class GameScene: SKScene {
     
     let scoreKey = "HighScoreKey"
     
-    private var lastUpdateTime : TimeInterval = 0
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
     
     override func sceneDidLoad() {
-        
-        self.lastUpdateTime = 0
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        
-        
-        scoreLabel = SKLabelNode(text: "Score: 0")
-        scoreLabel.position = CGPoint(x:  size.width/2 - 100, y: size.height/2 - 100)
-        scoreLabel.fontName = "AmericanTypewriter-Bold"
-        //http://iosfonts.com
-        scoreLabel.fontSize = 30
-        scoreLabel.fontColor = NSColor.white
+        scoreLabel = self.childNode(withName: "scoreText") as? SKLabelNode
         score = 0
         
-        highScoreLabel = SKLabelNode(text: "High Score: 0")
-        highScoreLabel.position = CGPoint(x:  size.width/2 - 875, y: size.height/2 - 100)
-        highScoreLabel.fontName = "AmericanTypewriter-Bold"
-        //http://iosfonts.com
-        highScoreLabel.fontSize = 30
-        highScoreLabel.fontColor = NSColor.white
+        highScoreLabel = self.childNode(withName: "highScoreText") as? SKLabelNode
         highScore = UserDefaults.standard.integer(forKey: scoreKey)
-        
-        self.addChild(scoreLabel)
-        self.addChild(highScoreLabel)
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func mouseDown(with event: NSEvent) {
-        self.touchDown(atPoint: event.location(in: self))
-    }
-    
-    override func mouseDragged(with event: NSEvent) {
-        self.touchMoved(toPoint: event.location(in: self))
-    }
-    
-    override func mouseUp(with event: NSEvent) {
-        self.touchUp(atPoint: event.location(in: self))
     }
     
     override func keyDown(with event: NSEvent) {
         switch event.keyCode {
-        case 0x31:
-            if let label = self.label {
-                label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-            }
         //press "i" -> keycode : 34
         case 34:
             score += 1
@@ -131,30 +48,10 @@ class GameScene: SKScene {
             if score > highScore {
                 highScore = score
                 UserDefaults.standard.set(score, forKey: scoreKey)
-//                defaults.set(score)
             }
             
         default:
             print("keyDown: \(event.characters!) keyCode: \(event.keyCode)")
         }
-    }
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-        
-        // Initialize _lastUpdateTime if it has not already been
-        if (self.lastUpdateTime == 0) {
-            self.lastUpdateTime = currentTime
-        }
-        
-        // Calculate time since last update
-        let dt = currentTime - self.lastUpdateTime
-        
-        // Update entities
-        for entity in self.entities {
-            entity.update(deltaTime: dt)
-        }
-        
-        self.lastUpdateTime = currentTime
     }
 }
