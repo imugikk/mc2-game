f8d11127073718b720be6290fe2e675357bdb07a
ugik <ugyparaptughessa@gmail.com>
2023-06-19 10:10:27 +0700

    collision

--- mc2/GameScene/GameScene.swift
+++ mc2/GameScene/GameScene.swift
@@ -11,12 +11,8 @@ import GameController
 
 enum bitMask: UInt32 {
     case player = 0x1
-    case ingredient_1 = 0x2
-    case ingredient_2 = 0x3
-    case ingredient_3 = 0x4
-    case square_1 = 0x5
-    case square_2 = 0x6
-    case square_3 = 0x7
+    case ingredient = 0x2
+    case square = 0x5
     case raycast = 0x8
 }
 
@@ -35,6 +31,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     var ingredientNode: SKSpriteNode?
     var ingredientNode2: SKSpriteNode?
     var ingredientNode3: SKSpriteNode?
+    var highlight: SKSpriteNode?
     
     var square_1: SKSpriteNode?
     var square_2: SKSpriteNode?
@@ -44,6 +41,8 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     var gameover: Bool = false
     var lastRayPos = CGPoint(x: 0, y: 0)
+    var leftJoystickInput1 = (x: 0.0, y: 0.0)
+    
     
     override func sceneDidLoad() {
         self.lastUpdateTime = 0
@@ -76,50 +75,58 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
         // Create your player physics body
         let playerBody = SKPhysicsBody(rectangleOf: CGSize(width: 88, height: 41))
-        playerBody.categoryBitMask = 0x1
-        playerBody.contactTestBitMask = 0x2
+        playerBody.categoryBitMask = bitMask.player.rawValue
+        playerBody.contactTestBitMask = bitMask.ingredient.rawValue
+        playerBody.collisionBitMask = bitMask.square.rawValue
         playerBody.isDynamic = true // Ensure player is dynamic
         playerBody.friction = 0 // Set friction to 0
+        playerBody.allowsRotation = false
         player.physicsBody = playerBody
 
         let ingredientBody = SKPhysicsBody(rectangleOf: CGSize(width: 50, height: 50))
-        ingredientBody.categoryBitMask = 0x2 // Unique bitmask for the ingredient body
-        ingredientBody.contactTestBitMask = 0x1 // Bitmask for bodies the enemy should detect contact with
+        ingredientBody.categoryBitMask = bitMask.ingredient.rawValue // Unique bitmask for the ingredient body
+        ingredientBody.contactTestBitMask = bitMask.player.rawValue // Bitmask for bodies the enemy should detect contact with
+        ingredientBody.collisionBitMask = 0
         ingredientBody.isDynamic = false // Ensure ingredient is dynamic
         ingredientBody.friction = 0 // Set friction to 0
         ingredientNode?.physicsBody = ingredientBody
         
         let ingredientBody2 = SKPhysicsBody(rectangleOf: CGSize(width: 50, height: 50))
-        ingredientBody2.categoryBitMask = 0x4 // Unique bitmask for the second ingredient body
-        ingredientBody2.contactTestBitMask = 0x1 // Bitmask for bodies the second ingredient should detect contact with
+        ingredientBody2.categoryBitMask = bitMask.ingredient.rawValue // Unique bitmask for the second ingredient body
+        ingredientBody2.contactTestBitMask = bitMask.player.rawValue // Bitmask for bodies the second ingredient should detect contact with
+        ingredientBody2.collisionBitMask = 0
         ingredientBody2.isDynamic = false // Ensure the second ingredient is not dynamic
         ingredientBody2.friction = 0 // Set friction to 0
         ingredientNode2?.physicsBody = ingredientBody2
         
         let ingredientBody3 = SKPhysicsBody(rectangleOf: CGSize(width: 50, height: 50))
-        ingredientBody3.categoryBitMask = 0x3 // Unique bitmask for the second ingredient body
-        ingredientBody3.contactTestBitMask = 0x1 // Bitmask for bodies the second ingredient should detect contact with
+        ingredientBody3.categoryBitMask = bitMask.ingredient.rawValue // Unique bitmask for the second ingredient body
+        ingredientBody3.contactTestBitMask = bitMask.player.rawValue // Bitmask for bodies the second ingredient should detect contact with
+        ingredientBody3.collisionBitMask = 0
         ingredientBody3.isDynamic = false // Ensure the second ingredient is not dynamic
         ingredientBody3.friction = 0 // Set friction to 0
         ingredientNode3?.physicsBody = ingredientBody3
-        
+
         let squareBody = SKPhysicsBody(rectangleOf: CGSize(width: 50, height: 50))
-        squareBody.categoryBitMask = 0x5 // Unique bitmask for the second ingredient body
-        squareBody.contactTestBitMask = 0x1 // Bitmask for bodies the second ingredient should detect contact with
+        squareBody.categoryBitMask = bitMask.square.rawValue // Unique bitmask for the second ingredient body
+        squareBody.contactTestBitMask = bitMask.raycast.rawValue // Bitmask for bodies the second ingredient should detect contact with
+        squareBody.collisionBitMask = bitMask.player.rawValue
         squareBody.isDynamic = false // Ensure the second ingredient is not dynamic
         squareBody.friction = 0 // Set friction to 0
         square_1?.physicsBody = squareBody
-        
+
         let squareBody2 = SKPhysicsBody(rectangleOf: CGSize(width: 50, height: 50))
-        squareBody2.categoryBitMask = 0x6 // Unique bitmask for the second ingredient body
-        squareBody2.contactTestBitMask = 0x1 // Bitmask for bodies the second ingredient should detect contact with
+        squareBody2.categoryBitMask = bitMask.square.rawValue // Unique bitmask for the second ingredient body
+        squareBody2.contactTestBitMask = bitMask.raycast.rawValue // Bitmask for bodies the second ingredient should detect contact with
+        squareBody2.collisionBitMask = bitMask.player.rawValue
         squareBody2.isDynamic = false // Ensure the second ingredient is not dynamic
         squareBody2.friction = 0 // Set friction to 0
         square_2?.physicsBody = squareBody2
-        
+
         let squareBody3 = SKPhysicsBody(rectangleOf: CGSize(width: 50, height: 50))
-        squareBody3.categoryBitMask = 0x7 // Unique bitmask for the second ingredient body
-        squareBody3.contactTestBitMask = 0x1 // Bitmask for bodies the second ingredient should detect contact with
+        squareBody3.categoryBitMask = bitMask.square.rawValue // Unique bitmask for the second ingredient body
+        squareBody3.contactTestBitMask = bitMask.raycast.rawValue // Bitmask for bodies the second ingredient should detect contact with
+        squareBody3.collisionBitMask = bitMask.player.rawValue
         squareBody3.isDynamic = false // Ensure the second ingredient is not dynamic
         squareBody3.friction = 0 // Set friction to 0
         square_3?.physicsBody = squareBody3
@@ -166,6 +173,18 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             bodyB.collisionBitMask = 0
         }
         
+//        if (categoryA == bitMask.raycast.rawValue && categoryB == bitMask.square_1.rawValue) {
+//            highlight = childNode(withName: "highlight") as? SKSpriteNode
+//            highlight?.position = contact.bodyB.node!.position
+//
+//            self.scene?.addChild(highlight!)
+//        } else {
+//            highlight?.removeFromParent()
+//        }
+        
+        // Enable collision between player and ingredient
+        bodyA.collisionBitMask = 1
+        bodyB.collisionBitMask = 1
         
     }
     
@@ -174,19 +193,12 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         let bodyB = contact.bodyB
         
         if (bodyA.node is Player && bodyB.node == (bodyA.node as? Player)?.ingredientNode) ||
-           (bodyB.node is Player && bodyA.node == (bodyB.node as? Player)?.ingredientNode) {
-            // Player body no longer in contact with ingredient body
-            (bodyA.node as? Player)?.removePickupPopup()
-        }
-        
-        if (bodyA.node is Player && bodyB.node == (bodyA.node as? Player)?.ingredientNode2) ||
-           (bodyB.node is Player && bodyA.node == (bodyB.node as? Player)?.ingredientNode2) {
-            // Player body no longer in contact with ingredient body
-            (bodyA.node as? Player)?.removePickupPopup()
-        }
-        
-        if (bodyA.node is Player && bodyB.node == (bodyA.node as? Player)?.ingredientNode3) ||
-           (bodyB.node is Player && bodyA.node == (bodyB.node as? Player)?.ingredientNode3) {
+           (bodyB.node is Player && bodyA.node == (bodyB.node as? Player)?.ingredientNode) ||
+            (bodyA.node is Player && bodyB.node == (bodyA.node as? Player)?.ingredientNode2) ||
+               (bodyB.node is Player && bodyA.node == (bodyB.node as? Player)?.ingredientNode2) ||
+            (bodyA.node is Player && bodyB.node == (bodyA.node as? Player)?.ingredientNode3) ||
+               (bodyB.node is Player && bodyA.node == (bodyB.node as? Player)?.ingredientNode3)
+        {
             // Player body no longer in contact with ingredient body
             (bodyA.node as? Player)?.removePickupPopup()
         }
@@ -198,15 +210,16 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
         player.movement(hInput: controllerInput.x, vInput: controllerInput.y, deltaTime: deltaTime)
         
-//        let lastPos = (xDirection == 0 && yDirection == 0) ? lastRayPos : player.position
-//        let rayPos = CGPoint(x: lastPos.x + xDirection * 70, y: lastPos.y + yDirection * 70)
+        let lastPos = player.position
+        let rayPos = CGPoint(x: lastPos.x + 70, y: lastPos.y + 70)
         
-//        ray = SKPhysicsBody(circleOfRadius: 10, center: rayPos)
-//        ray.categoryBitMask = bitMask.raycast.rawValue
-//        ray.contactTestBitMask = bitMask.player.rawValue
-//        ray.collisionBitMask = bitMask.square_1.rawValue
-//        physicsBody = ray
-//        lastRayPos = rayPos
+        ray = SKPhysicsBody(circleOfRadius: 10, center: rayPos)
+        ray.categoryBitMask = bitMask.raycast.rawValue
+        ray.contactTestBitMask = bitMask.square.rawValue
+        ray.collisionBitMask = 0
+        ray.isDynamic = true
+        physicsBody = ray
+        lastRayPos = rayPos
     }
     
     //calculate the time difference between the current and previous frame
