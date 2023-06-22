cb04323c667b233aeda38523baac47fa703f2f5f
tiff12 <tiffanyindryani@gmail.com>
2023-06-22 09:16:18 +0700

    visual component

---
+++ mc2/Components/VisualComponent.swift
@@ -0,0 +1,24 @@
+////
+////  VisualComponent.swift
+////  mc2
+////
+////  Created by Tiffany Angela Indryani on 21/06/23.
+////
+//
+//import SpriteKit
+//import GameplayKit
+//
+//class Entity: GKEntity {}
+//
+//class VisualComponent: GKComponent {
+//    let node: SKSpriteNode
+//
+//    init(node: SKSpriteNode) {
+//        self.node = node
+//        super.init()
+//    }
+//    
+//    required init?(coder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
+//}
